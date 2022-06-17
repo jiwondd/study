b0f7e90e6c622b4b3a7ab3bc3810847f9845884b
jiwondd <107660315+jiwondd@users.noreply.github.com>
2022-06-17 14:10:11 +0900

    Add files via upload

---
+++ keras01.py
@@ -0,0 +1,29 @@
+#1.데이터
+import numpy as np  
+x=np.array([1,2,3])
+y=np.array([1,2,3])
+
+#2.모델구성
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import Dense             
+
+model=Sequential()
+model.add(Dense(4,input_dim=1))
+model.add(Dense(5))
+model.add(Dense(3))
+model.add(Dense(2))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss='mse',optimizer='adam')
+model.fit(x,y,epochs=500)
+
+#4. 평가, 예측
+loss=model.evaluate(x,y)
+print('loss:',loss)
+
+result=model.predict([4])
+print('4의 예측값:',result)
+
+# loss: 0.0015836921520531178
+# 4의 예측값: [[4.0808353]]
