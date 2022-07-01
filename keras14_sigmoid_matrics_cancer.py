9269d4a71d82dc431431439006a0ccb1589e0b34
Jiwon Lim <107660315+jiwondd@users.noreply.github.com>
2022-07-01 17:41:26 +0900

    Add files via upload

--- keras14_sigmoid_matrics_cancer.py
+++ keras14_sigmoid_matrics_cancer.py
@@ -25,11 +25,11 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,
         train_size=0.8,shuffle=True, random_state=777)
 
 model=Sequential()
-model.add(Dense(50,activation='linear',input_dim=30))
-model.add(Dense(80,activation='linear')) 
-model.add(Dense(50,activation='linear'))
-model.add(Dense(20,activation='linear'))
-model.add(Dense(10,activation='linear'))
+model.add(Dense(50,input_dim=30))
+model.add(Dense(80,activation='sigmoid')) 
+model.add(Dense(50,activation='relu')) #렐루는 중간 레이어에서만 가능함 / 정확도 80퍼이상
+model.add(Dense(20,activation='sigmoid'))
+model.add(Dense(10,activation='sigmoid'))
 model.add(Dense(1,activation='sigmoid'))
 
 #3.컴파일, 훈련
@@ -42,15 +42,19 @@ hist=model.fit(x_train,y_train, validation_split=0.2, callbacks=[earlyStopping],
 
 #4. 평가, 예측
 loss=model.evaluate(x_test,y_test)
-print('loss:',loss)
-print('=========================================')
-print(hist.history['val_loss'])
 
+# print('loss:',loss)
+# print('=========================================')
+# print(hist.history['val_loss'])
 
-# y_predict=model.predict(x_test)
-# from sklearn.metrics import r2_score
+y_predict=model.predict(x_test)
+from sklearn.metrics import r2_score, accuracy_score
+print(y_predict)
+
+# [과제] accuracy 값을 완성하자
 # r2=r2_score(y_test,y_predict)
-# print('r2스코어:',r2)
+# acc=accuracy_score(y_test,y_predict)
+# print('acc score:',acc)
 # ㄴR2는 회귀모델에 쓰이는 평가지표니까 안쓴다!
 '''
 # loss: 0.11057772487401962
@@ -60,7 +64,6 @@ print(hist.history['val_loss'])
 
 # loss: [0.1370796114206314, 0.9385964870452881]
 #        ㄴ얘는 원래 LOSS  /  ㄴ매트릭스의 값 (평가지표 두개[리스트]니까 두개나온거임)
-
 
 
 '''
