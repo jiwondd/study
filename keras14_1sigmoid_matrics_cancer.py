b99896f1837b23631289586fddc111c3c0215f9b
Jiwon Lim <107660315+jiwondd@users.noreply.github.com>
2022-07-03 16:03:02 +0900

    Update keras14_1sigmoid_matrics_cancer.py

--- keras14_1sigmoid_matrics_cancer.py
+++ keras14_1sigmoid_matrics_cancer.py
@@ -5,6 +5,7 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.callbacks import EarlyStopping
+from sklearn.metrics import accuracy_score
 
 
 #1. 데이터
@@ -23,10 +24,10 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,
         train_size=0.8,shuffle=True, random_state=777)
 
 model=Sequential()
-model.add(Dense(50,input_dim=30))
-model.add(Dense(80,activation='sigmoid')) 
+model.add(Dense(60,input_dim=30))
+model.add(Dense(90,activation='relu')) 
 model.add(Dense(50,activation='relu')) #렐루는 중간 레이어에서만 가능함 / 정확도 80퍼이상
-model.add(Dense(20,activation='sigmoid'))
+model.add(Dense(20,activation='relu'))
 model.add(Dense(10,activation='sigmoid'))
 model.add(Dense(1,activation='sigmoid'))
 
@@ -42,14 +43,42 @@ hist=model.fit(x_train,y_train, validation_split=0.2, callbacks=[earlyStopping],
 loss=model.evaluate(x_test,y_test)
 print("loss : ",loss)
 y_predict=model.predict(x_test)
-print(y_predict)
+
+y_predict = model.predict(x_test)
+# print(x_test.shape)
+
+
+y_predict[(y_predict<0.5)]=0  
+y_predict[(y_predict>=0.5)]=1  
+
+# print(y_predict)
+# print(y_predict.shape)
+
+
+acc = accuracy_score(y_test, y_predict)
+print('acc score :', acc)
 
 # [과제] accuracy 값을 완성하자
 # r2=r2_score(y_test,y_predict)
-# acc=accuracy_score(y_test,y_predict)
-# print('acc score:',acc)
 # ㄴR2는 회귀모델에 쓰이는 평가지표니까 안쓴다!
 
 
+'''
+loss :  [0.1798676997423172, 0.9122806787490845]
+acc score : 0.8508771929824561 
+
+loss :  [0.15738441050052643, 0.9298245906829834]
+acc score : 0.9298245614035088 -> 노드갯수 인풋딤보다 2배늘림, 액티베이션 마지막빼고 렐루
+
+loss :  [0.154058575630188, 0.9385964870452881]
+acc score : 0.9385964912280702 -> 마지막이랑 바로 위에 액티베이션 시그모이드 나머지 렐루
+
+loss :  [0.17140837013721466, 0.9473684430122375]
+acc score : 0.9473684210526315 -> 같은조건으로 한번 더 
 
 
+
+
+
+
+'''
