57864335810170ff80389ecfbe68a5cc96057c0e
Jiwon Lim <107660315+jiwondd@users.noreply.github.com>
2022-07-17 17:44:38 +0900

    Add files via upload

--- keras45_kaggle_jena_LSTM.py
+++ keras45_kaggle_jena_LSTM.py
@@ -1,23 +1,115 @@
-# RNN, CNN 사용해서 evlauate까지만
-
 import numpy as np
 import pandas as pd
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, SimpleRNN, LSTM
+from tensorflow.python.keras.layers import Dense, LSTM, Dropout
 from sklearn.model_selection import train_test_split
-from tensorflow.python.keras.callbacks import EarlyStopping
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.preprocessing import MinMaxScaler,StandardScaler
-from sklearn.preprocessing import MaxAbsScaler,RobustScaler
-from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+import math
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+import datetime
+import time
+from sklearn.model_selection import train_test_split
+import inspect, os
+a = inspect.getfile(inspect.currentframe()) #현재 파일이 위치한 경로 + 현재 파일 명
+print(a)
+print(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))) #현재 파일이 위치한 경로
+print(a.split("\\")[-1]) #현재 파일 명
+current_name = a.split("\\")[-1]
+# filepath경로에 추가로  + current_name + '/' 삽입
 
 
-#1.데이터
-path = './kaggle_jena/' 
-train_set = pd.read_csv(path + 'train.csv', index_col=0)
-test_set = pd.read_csv(path + 'test.csv', index_col=0)
-# print(train_set) # [891 rows x 11 columns]
-# print(train_set.describe())
-# print(train_set.info())
+path = './_data/kaggle_jena/'
+df_weather=pd.read_csv(path + 'jena_climate_2009_2016.csv', index_col=0)
+df_weather.describe()
+
+print(df_weather.columns)
+
+scaler = MinMaxScaler()
+scale_cols = ['p (mbar)', 'T (degC)', 'Tpot (K)', 'Tdew (degC)', 'rh (%)',
+       'VPmax (mbar)', 'VPact (mbar)', 'VPdef (mbar)', 'sh (g/kg)',
+       'H2OC (mmol/mol)', 'rho (g/m**3)', 'wv (m/s)', 'max. wv (m/s)',
+       'wd (deg)']
+df_scaled = scaler.fit_transform(df_weather[scale_cols])
+
+df_scaled = pd.DataFrame(df_scaled)
+df_scaled.columns = scale_cols
+
+print(df_scaled)
+
+# 데이터셋 구성하기
+TEST_SIZE = 200
+
+train = df_scaled[:-TEST_SIZE]
+test = df_scaled[-TEST_SIZE:]
+
+def make_dataset(data, label, window_size=20):
+    feature_list = []
+    label_list = []
+    for i in range(len(data) - window_size):
+        feature_list.append(np.array(data.iloc[i:i+window_size]))
+        label_list.append(np.array(label.iloc[i+window_size]))
+    return np.array(feature_list), np.array(label_list)
+
+feature_cols = ['p (mbar)', 'Tpot (K)', 'Tdew (degC)', 'rh (%)',
+       'VPmax (mbar)', 'VPact (mbar)', 'VPdef (mbar)', 'sh (g/kg)',
+       'H2OC (mmol/mol)', 'rho (g/m**3)', 'wv (m/s)', 'max. wv (m/s)',
+       'wd (deg)']
+label_cols = ['T (degC)']
+
+train_feature = train[feature_cols]
+train_label = train[label_cols]
+
+
+# train dataset
+train_feature, train_label = make_dataset(train_feature, train_label, 20)
+
+# train, validation set 생성
+x_train, x_test, y_train, y_test = train_test_split(train_feature, train_label, test_size=0.2)
+
+# print(x_train.shape, x_valid.shape)   #(336264, 20, 13) (84067, 20, 13)
+
+#2.모델 구성
+model = Sequential()
+model.add(LSTM(32,input_shape=(train_feature.shape[1], train_feature.shape[2]), 
+               activation='relu',return_sequences=True))
+model.add(LSTM(64,activation='relu'))
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(256,activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(128,activation='relu'))
+model.add(Dense(64,activation='linear'))
+model.add(Dense(32,activation='linear'))
+model.add(Dense(1))
+
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+earlyStopping = EarlyStopping(monitor='val_loss', patience=100, mode='min', verbose=1,
+                restore_best_weights=True)
+date = datetime.datetime.now()
+date = date.strftime("%m%d_%H%M") 
+print(date)
+save_filepath = './_ModelCheckPoint/' + current_name + '/'
+load_filepath = './_ModelCheckPoint/' + current_name + '/'
+filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
+mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, save_best_only=True, 
+                      filepath= "".join([save_filepath, date, '_', filename])
+                      )
+
+start_time = time.time()
+hist = model.fit(x_train, y_train, epochs=1000, 
+                 batch_size=10024, validation_split=0.2, 
+                 callbacks=[earlyStopping, mcp], 
+                 verbose=1) 
+
+end_time = time.time() - start_time
+
+
+# 4. 평가, 예측
+loss = model.evaluate(x_test, y_test)
+print('loss : ', loss)
+
+# loss :  0.001036290661431849
