9add368a71f2ca2b8854e6363b8d283e4e5cee74
Jiwon Lim <107660315+jiwondd@users.noreply.github.com>
2022-09-13 12:07:24 +0900

    Add files via upload

---
+++ AE/a01_autoencoder.py
@@ -0,0 +1,57 @@
+import numpy as np
+from keras.datasets import mnist
+
+(x_train, _), (x_test, _) = mnist.load_data()
+#인풋 아웃풋이 같다, 특성이 가장 두드러진 부분이 뽑아져나온다. (얼굴을 넣으면 눈코입이 특성이니까 나오고 주근깨 여드름은 지워짐 좋은데?)
+
+x_train=x_train.reshape(60000, 784).astype('float32')/255.
+x_test=x_test.reshape(10000, 784).astype('float32')/255.
+
+from keras.models import Sequential, Model
+from keras.layers import Dense, Input
+
+input_img=Input(shape=(784,))
+
+encoded=Dense(64,activation='relu')(input_img)
+#             ㄴ이 숫자는 줄일수도 늘일수도 있다. 이 과정에서 중요한 특성은 남고 필요없는 애들은 사라진다.
+# 큰노드에서 작은노드 거쳐서 다시 원래대로
+
+# encoded=Dense(1064,activation='relu')(input_img) #노드를 늘여보자 / 족금 좋아진거같기두?
+# encoded=Dense(16,activation='relu')(input_img) #노드를 줄여보자 / 그림이 많이 흐릿해졌다
+
+decoded=Dense(784,activation='sigmoid')(encoded)
+# 위에서 /255 해줘서 0에서 1사이로 되어있기때문에 시그모이드 적용해줌(다른 데이터에서는 액티베이숀 다른거 넣어도 됨) 
+# decoded=Dense(784,activation='relu')(encoded) #액티베이션 렐루로 바꿔보자 / 그림이 많이 지저분해졌다.
+# decoded=Dense(784,activation='linear')(encoded)#액티베이션 리니어로 바꿔보자 / 
+# decoded=Dense(784,activation='tanh')(encoded)#액티베이션 탄(-1 ~ 1) 바꿔보자 / 
+autoencoder=Model(input_img,decoded)
+# autoencoder.summary()
+
+
+# 컴파일 훈련
+# autoencoder.compile(optimizer='adam',loss='binary_crossentropy')
+# autoencoder.compile(optimizer='adam',loss='mse')
+autoencoder.compile(optimizer='adam',loss='binary_crossentropy',metrics=['acc'])
+
+autoencoder.fit(x_train,x_train,epochs=30,batch_size=128,validation_split=0.2)
+# 여기서는 y값이 없어서 훈련시킬때 x값으로 (준지도학습)
+decoded_imgs=autoencoder.predict(x_test)
+
+import matplotlib.pyplot as plt
+n=10
+plt.figure(figsize=(20,4))
+for i in range(n):
+    ax=plt.subplot(2,n,i+1)
+    plt.imshow(x_test[i].reshape(28,28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    
+    ax=plt.subplot(2,n,i+1+n)
+    plt.imshow(decoded_imgs[i].reshape(28,28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+
+plt.show()
---
+++ AE/a02_ae.py
@@ -0,0 +1,67 @@
+from lightgbm import plot_tree
+import numpy as np
+from keras.datasets import mnist
+from sklearn.decomposition import PCA
+
+(x_train, _), (x_test, _) = mnist.load_data()
+#인풋 아웃풋이 같다, 특성이 가장 두드러진 부분이 뽑아져나온다. (얼굴을 넣으면 눈코입이 특성이니까 나오고 주근깨 여드름은 지워짐 좋은데?)
+
+x_train=x_train.reshape(60000, 784).astype('float32')/255.
+x_test=x_test.reshape(10000, 784).astype('float32')/255.
+
+################################################
+pca=PCA(n_components=12)
+x=pca.fit_transform(x_train)
+pca_EVR=pca.explained_variance_ratio_
+cumsum=np.cumsum(pca_EVR)
+print(np.argmax(cumsum>=0.95)+1) #154
+print(np.argmax(cumsum>=0.99)+1) #331
+print(np.argmax(cumsum>=0.999+1)) #486
+print(np.argmax(cumsum>=1.0)+1) #713
+#################################################
+
+from keras.models import Sequential, Model
+from keras.layers import Dense, Input
+
+def autoencoder(hidden_layer_size):
+    model=Sequential()
+    model.add(Dense(units=hidden_layer_size,input_shape=(784,),
+                    activation='relu'))
+    model.add(Dense(units=784,activation='sigmoid'))
+    return model
+
+# 01번이랑 똑같은데 "함수"형으로 만든거임
+# model=autoencoder(hidden_layer_size=64)
+model=autoencoder(hidden_layer_size=154) #PCA의 95%성능
+model=autoencoder(hidden_layer_size=331) #PCA의 99%성능
+
+model.compile(optimizer='adam',loss='binary_crossentropy')
+model.fit(x_train,x_train,epochs=1)
+output=model.predict(x_test)
+
+import matplotlib.pyplot as plt
+import random
+
+fig,((ax1,ax2,ax3,ax4,ax5),(ax6,ax7,ax8,ax9,ax10))=plt.subplots(2,5,figsize=(20,7))
+    
+random_images=random.sample(range(output.shape[0]),5)
+
+for i, ax in enumerate([ax1,ax2,ax3,ax4,ax5]):
+    ax.imshow(x_test[random_images[i]].reshape(28,28),cmap='gray')
+    if i==0:
+        ax.set_ylabel("INPUT",size=20)
+    ax.grid(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    
+for i, ax in enumerate([ax6,ax7,ax8,ax9,ax10]):
+    ax.imshow(output[random_images[i]].reshape(28,28),cmap='gray')
+    if i==0:
+        ax.set_ylabel("OUTPUT",size=20)
+    ax.grid(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    
+plt.tight_layout()
+plt.show()
+
